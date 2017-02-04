eff6402794c46273185620831a4c30a538ce2237
git_user <patronumme@gmail.com>
2017-02-04 20:59:36 +0800

    change build path

--- build.pl
+++ build.pl
@@ -4,8 +4,9 @@ use Cwd;
 print @ARGV[0];
 
 my $pwd = getcwd();
-my $cmake = "$pwd/cmake/bin/cmake";
-my $ninja = "$pwd/ninja";
+my $binary = "$pwd/Third/Binary";
+my $cmake = "$binary/cmake/bin/cmake";
+my $ninja = "$binary/ninja";
 
 mkdir "Built";
 chdir "./Built";
@@ -13,7 +14,7 @@ chdir "./Built";
 my $VsEnv = "\"C:\\Program Files (x86)\\Microsoft Visual Studio 14.0\\VC\\vcvarsall.bat\"";
 
 my $path = $ENV{"PATH"};
-$ENV{"PATH"} = "$path;$pwd";
+$ENV{"PATH"} = "$path;$binary";
 
 my $cmd = "$VsEnv x64 && $cmake -GNinja $pwd && $ninja";
 print "Building command - '$cmd'\n\n";
