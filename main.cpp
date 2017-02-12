c672ad67414714645caec74933221fe5affc1149
git_user <patronumme@gmail.com>
2017-02-12 16:19:01 +0800

    QTree sample

--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
 #include <stdio.h>
+
 #include <QtWidgets/QApplication>
+#include <QtWidgets/QDirModel>
+#include <QtWidgets/QSplitter>
+#include <QtWidgets/QTreeView>
 
 int main(int argc, char** argv)
 {
     QApplication app(argc, argv);
-    printf("Hello cmake!\n");
-    return 0;
+    QSplitter *splitter = new QSplitter;  
+    QDirModel *model = new QDirModel;  
+
+    QTreeView *tree = new QTreeView(splitter);  
+    tree->setModel(model);  
+    tree->setRootIndex(model->index("c:\\"));
+
+    QString a("cate");
+    splitter->setWindowTitle(a);  
+    splitter->show();  
+    return app.exec();
 }
